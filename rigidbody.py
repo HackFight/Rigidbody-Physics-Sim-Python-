7023b0420bff7de1d144b3342b27e2016db20b28
HackFight <hackfightowo@gmail.com>
2024-06-26 16:12:33 +0100

    Implementation of simple graphics with Pygame.

--- rigidbody.py
+++ rigidbody.py
@@ -1,8 +1,19 @@
-import time
 import vectors
 from vectors import Vector2D
 from vectors import Magnitude
 import pygame
+
+#initialise pygame
+pygame.init()
+#screen size
+scr = (width, height) = (1000, 1000)
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Physics Simulator")
+
+# Colors
+white = (252, 251, 244)
+black = (20, 25, 24)
+purple = (186, 85, 255)
 
 class Rigidbody2D():
     def __init__(self, pos, mass):
@@ -42,19 +53,17 @@ class Rigidbody2D():
 
         self.pos = vectors.Add(self.pos, s)
         self.velocity = v
+    
+    def Draw(self):
+        pygame.draw.circle(screen, purple, (self.pos.x, self.pos.y), 5)
 
 
 ball = Rigidbody2D(Vector2D(0.0, 0.0), 1.0)
-ball.SetPostion(Vector2D(5.0, 0.0))
-ball.SetVelocity(Vector2D(0.0, -3.0))
+ball.SetPostion(Vector2D(600.0, 500.0))
+ball.SetVelocity(Vector2D(0.0, 50.0))
 
 f_centripetal = Vector2D(0.0, 0.0)
 
-#initialise pygame
-pygame.init()
-#screen size
-scr = (width, height) = (960, 960)
-screen = pygame.display.set_mode((width, height))
 #prepare game loop
 game = True
 #init clock for fps manipulation
@@ -62,22 +71,40 @@ clock = pygame.time.Clock()
 fps = 60
 
 while game:
-    clock.tick(fps)
+    deltaTime = clock.tick(fps) / 1000
 
-    print(time.clock())
-
+    ############# Inputs
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+            break
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE: # ends the game, can be a different key if desired. The program will run fine without this
+                game = False
+                break
+    
+    ############# Simulation
+    # Reset the forces
     forces = []
 
-    F = (ball.mass * Magnitude(ball.velocity)**2) / 5
-    f_centripetal = vectors.Multiply(vectors.Normalize(vectors.Subtract(Vector2D(0.0, 0.0), ball.pos)), F)
+    # Calculate the centripetal force F=(mv^2)/r
+    F = (ball.mass * Magnitude(ball.velocity)**2) / 100
+    f_centripetal = vectors.Multiply(vectors.Normalize(vectors.Subtract(Vector2D(500.0, 500.0), ball.pos)), F)
     forces.append(f_centripetal)
 
+    # Set the forces of the rb
     ball.forces = forces
 
-    ball.Move(deltatime)
-    #print(str(ball.pos.x) + ", " + str(ball.pos.y))
+    #Tick the rb
+    ball.Move(deltaTime)
 
-    time.sleep(1/60)
+    ########## Render
+    #Clear screen
+    screen.fill(black)
+
+    #Draw rb
+    ball.Draw()
+
     #end of loop
     pygame.display.flip()
 
